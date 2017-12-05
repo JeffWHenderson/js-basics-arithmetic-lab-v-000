9aee91ae716c405bedc658c1c5e9e06f059b6e10
jeffrey henderson <jeffrey.henderson17@gmail.com>
2017-12-05 17:17:14 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -4,4 +4,4 @@
 let newID = oldID + 1000000000;
 let ageIsValid = Number.isInteger(currentAge)
 let randomNumber = Math.random(0, 20);
-let randomInteger = Math.floor.random();
+let randomInteger = Math.floor(Math.random());
